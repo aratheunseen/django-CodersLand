880ab5619754e1e59f1ccabc2c0ae981940646e9
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2023-06-28 20:44:21 +0600

    update

--- main/config.py
+++ main/config.py
@@ -10,7 +10,7 @@ META_DESCRIPTION = "CodersLand is a platform where we are showcasing all coders 
 META_KEYWORDS = "coders portfolio, codersland, github users portfolio"
 
 # GitHub API Details
-GITHUB_PAT = "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"
+GITHUB_PAT = "YOUR_GITHUB_PAT"
 GITHUB_API_URL = "https://api.github.com"
 GITHUB_API_HEADERS = {
     'Authorization': f'Token {GITHUB_PAT}',
